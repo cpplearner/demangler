562e118c8c45ea2c6ef5a76fa5d8894be55643be
cpplearner <cpplearner@outlook.com>
2025-06-06 13:39:47 +0800

    Update test

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,8 +1,24 @@
-// OPTIONS: /GF /Zc:threadSafeInit-
+// OPTIONS: /GF /Zc:threadSafeInit- /Zc:u8EscapeSequence
 
 // MANGLED: ??_C@_07MINAMJIC@hello?4?6@
 // DEMANGLED: 'string'
-auto str = "hello.\n";
+auto str1 = "hello.\n";
+
+// MANGLED: ??_C@_1BA@OMDLLEGE@?$AAh?$AAe?$AAl?$AAl?$AAo?$AA?4?$AA?6@
+// DEMANGLED: 'string'
+auto str2 = L"hello.\n";
+
+// MANGLED: ??_C@_08BKGFADIE@hello?4?6?$AB@
+// DEMANGLED: 'string'
+auto str3 = u8"hello.\n\u0001";
+
+// MANGLED: ??_C@_1BC@HFCBFCGI@?$AAh?$AAe?$AAl?$AAl?$AAo?$AA?4?$AA?6?$AA?$AB@
+// DEMANGLED: 'string'
+auto str4 = u"hello.\n\u0001";
+
+// MANGLED: ??_C@_2CE@HBHIHJLO@?$AA?$AA?$AAh?$AA?$AA?$AAe?$AA?$AA?$AAl?$AA?$AA?$AAl?$AA?$AA?$AAo?$AA?$AA?$AA?4?$AA?$AA?$AA?6@
+// DEMANGLED: 'string'
+auto str5 = U"hello.\n\u0001";
 
 struct type1 {
     type1() {}
